636d8105ef026f0cd74272223cb1015810c68335
Daniel <danielcgilibert@gmail.com>
2021-08-14 20:39:44 +0200

    v

--- Backend/controllers/usuariosController.js
+++ Backend/controllers/usuariosController.js
@@ -5,46 +5,83 @@ const bcrypt = require("bcrypt");
 const crearUsuario = async (req, res = response) => {
   const { name, email, password } = req.body;
 
-  const sqlComprobacion = `SELECT email FROM usuarios WHERE email='${email}'`;
 
-  await connection.query(sqlComprobacion)
-  .then(([result]) => {
-    console.log(result);
-    if (result.length > 0) {
-      res.status(400).json({
-        ok: false,
-        msg: "Un usuario ya existe con ese correo",
-      });
-    } else {
-      // Encriptar contraseña
-      const salt = bcrypt.genSaltSync();
-      const passwordEncriptado = bcrypt.hashSync(password, salt);
+  try {
+    const sqlComprobacion = `SELECT email FROM usuarios WHERE email='${email}'`;
 
-      const sql = `INSERT INTO usuarios (nombre, email, password) VALUES ('${name}', '${email}', '${passwordEncriptado}')`;
-      connection
-        .query(sql)
-        .then(([result]) => {
-          res.status(201).json({
-            ok: true,
-            msg: "registro",
-          });
-        })
-        .catch((err) => {
-          console.log(sql);
-          res.send(400);
-          console.log(err.message);
+    await connection.query(sqlComprobacion)
+    .then(([result]) => {
+      console.log(result);
+      if (result.length > 0) {
+        res.status(400).json({
+          ok: false,
+          msg: "Un usuario ya existe con ese correo",
         });
-    }
-  });
+      } else {
+        // Encriptar contraseña
+        const salt = bcrypt.genSaltSync();
+        const passwordEncriptado = bcrypt.hashSync(password, salt);
+  
+        const sql = `INSERT INTO usuarios (nombre, email, password) VALUES ('${name}', '${email}', '${passwordEncriptado}')`;
+        connection
+          .query(sql)
+          .then(([result]) => {
+            res.status(201).json({
+              ok: true,
+              msg: "registro",
+            });
+          })
+          .catch((err) => {
+            console.log(sql);
+            res.send(400);
+            console.log(err.message);
+          });
+      }
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: "Por favor hable con el administrador",
+    });
+  }
+ 
 };
 
-const loginUsuario = (req, res = response) => {
-  const { email, password } = req.body;
+const loginUsuario = async(req, res = response) => {
+  const { email, password:passwordUsuario } = req.body;
 
-  res.json({
-    ok: true,
-    msg: "login",
-  });
+  try {
+    const sqlComprobacion = `SELECT email,password FROM usuarios WHERE email='${email}'`;
+
+    await connection.query(sqlComprobacion)
+    .then(([result]) => {
+      if (!result.length > 0) {
+        res.status(400).json({
+          ok: false,
+          msg: "El usuario no existe con ese email",
+        });
+      }else{
+        const [{email, password}] = result
+        const validPassword = bcrypt.compareSync(password,passwordUsuario)
+        console.log(password);
+        console.log(validPassword);
+
+      }
+
+
+    });
+    
+    
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: "Por favor hable con el administrador",
+    });
+  }
+
+  
 };
 
 const renewTokeUsuario = (req, res = response) => {
